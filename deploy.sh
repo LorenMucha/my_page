e934c155218ace081b6bab514f381eb8cb1977ca
lorenmucha <>
2020-11-04 18:28:49 +0100

    new content

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
-#!/bin/bash
 rm -rf docs
 hugo 
 mv -f public docs
+sed -i 's/class="powered-by"/class="powered-by" style="display:none;"/g' index.html
+git config --global user.name "lorenmucha"
+git config --global user.password 'XbW_``bNk=(tYD%W6`B]'
+git add .
+git commit -m"new content"
+git push -f
