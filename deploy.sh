bf74db8a9f0fe74b4e53ec702c529c6ecaa27fa1
lorenmucha <>
2020-12-01 12:04:00 +0100

    update page

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,11 @@
 rm -rf docs
-hugo 
+hugo
 mv -f public docs
 cd docs
 # remove brand
 sed -i 's/class="powered-by"/class="powered-by" style="display:none;"/g' index.html
 # add formspree support
-
 sed -i 's,action="https://formspree.io/", action="https://formspree.io/f/xnqozbwk",g' index.html
+
+git commit -a -m"update page"
+git push
