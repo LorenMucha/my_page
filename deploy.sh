1c483bc40e0738a9cff3eec395a2e7738bff82b0
lorenmucha <>
2021-01-16 11:41:20 +0100

    fix formspree

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,3 @@ cd docs
 sed -i 's/class="powered-by"/class="powered-by" style="display:none;"/g' index.html
 # add formspree support
 sed -i 's,action="https://formspree.io/", action="https://formspree.io/f/xnqozbwk",g' index.html
-
-git commit -a -m"update page"
-git push -f
