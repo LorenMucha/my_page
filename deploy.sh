4ff2a23e4cb1b7eb1d3c3bd8b14eda6ef619583f
lorenmucha <>
2021-03-02 19:45:11 +0100

    add CNAME in deployment

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
 rm -rf docs
 hugo
 mv -f public docs
+# copy CNAME in docs directory
+cat CNAME >> docs/CNAME
 cd docs
 # remove brand
 sed -i 's/class="powered-by"/class="powered-by" style="display:none;"/g' index.html
